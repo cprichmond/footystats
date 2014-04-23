e967c4411b06ac3e845ea6d6fa87f4333312099d
Chris Richmond <cprichmond@gmail.com>
2014-04-23 22:45:20 +1000

    Added a menu and cleaned up some old views

--- app/js/controllers/mainController.js
+++ app/js/controllers/mainController.js
@@ -1,13 +1,23 @@
 'use strict';
 
-var mainCtrl = function(statsDataService){
-	
+var mainCtrl = function($route, $rootScope, statsDataService){
+	var _this = this;
+
+	this.showClearAllButton = false;
+
 	this.remove = function(){
 		statsDataService.remove();
 	}
 
+	$rootScope.$on('$routeChangeSuccess', function(ev, current, prev){
+		_this.showClearAllButton = false;
+		if(current.loadedTemplateUrl === "partials/entry.html"){
+			_this.showClearAllButton = true;
+		}
+	});
+
 };
 
-mainCtrl.$inject = ['statsDataService'];
+mainCtrl.$inject = ['$route', '$rootScope', 'statsDataService'];
 angular.module('footyStats.controllers').controller('MainCtrl', mainCtrl);
 
